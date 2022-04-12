8b0b93dd2f19acbc8c13ca181c50918d4e6502ed
Germán Hesles <german.hesles.hernandez@gmail.com>
2022-04-12 22:17:34 +0200

    Add list_helper
    Co-authored-by: Luis Hdez <luis.munoz.hdez@gmail.com>

---
+++ utils/list_helper.js
@@ -0,0 +1,34 @@
+const { all } = require("express/lib/application")
+
+const dummy = (blog) => {
+  return 1
+}
+
+const totalLikes = (blogs) => {
+  if (blogs.length === 0) {
+    return 0
+  }
+
+  const sumLikes = blogs.reduce((sum, blog) => {
+    return sum + blog.likes
+  }, 0)
+  return sumLikes
+}
+
+const favoriteBlog = (blogs) => {
+  const allLikes = blogs.map((blog) => {
+    return blog.likes
+  })
+
+  const maxLikes = Math.max(...allLikes)
+
+  const position = allLikes.indexOf(maxLikes)
+
+  return blogs[position]
+}
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog
+}
